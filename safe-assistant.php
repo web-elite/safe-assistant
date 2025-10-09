1e6f3f7b3c699c22a47a4fe8b561be33f3e40254
AlirezaYaghouti <webelitee@gmail.com>
2025-10-09 15:03:28 +0330

    release 1.6.6

--- safe-assistant.php
+++ safe-assistant.php
@@ -16,7 +16,7 @@
  * Plugin Name:       Safe Assistant
  * Plugin URI:        https://iranqq.com
  * Description:       Safe Assistant Sales assistant and necessary and efficient tools
- * Version:           1.6.5
+ * Version:           1.6.6
  * Requires at least: 5.2
  * Requires PHP:	  7.4
  * Author:            AlirezaYaghouti
